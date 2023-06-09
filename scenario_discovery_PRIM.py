d97302e86fa99fde0850cd69056f18e13796bed7
florihu <131660812+florihu@users.noreply.github.com>
2023-06-09 10:08:16 +0200

    water level min max included now really

--- scenario_discovery_PRIM.py
+++ scenario_discovery_PRIM.py
@@ -31,19 +31,22 @@ if __name__ == '__main__':
     levers = copy.deepcopy(dike_model.levers)
 
 
-    # Experiment: 20 Random policies
-    ema_logging.log_to_stderr(ema_logging.INFO)
 
-    # running the model through EMA workbench
 
-    scenarios = 1000
-    policies = 20
-
-    with MultiprocessingEvaluator(dike_model) as evaluator:
-        results = evaluator.perform_experiments(scenarios, policies)
-
-    experiments, outcomes = results
-
+    #
+    # # Experiment: 20 Random policies
+    # ema_logging.log_to_stderr(ema_logging.INFO)
+    #
+    # # running the model through EMA workbench
+    #
+    # scenarios = 1000
+    # policies = 20
+    #
+    # with MultiprocessingEvaluator(dike_model) as evaluator:
+    #     results = evaluator.perform_experiments(scenarios, policies)
+    #
+    # experiments, outcomes = results
+    #
 
 
 
